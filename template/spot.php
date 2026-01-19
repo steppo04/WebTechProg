0fbf65840ee5941652de44ffa8cf3ff4d8a3c866
Leonardo Meloni <leo.2404m@gmail.com>
2026-01-19 18:27:38 +0100

    comment fix

--- template/spot.php
+++ template/spot.php
@@ -79,7 +79,7 @@
                                 <?php endif; ?>
 
                                 <div class="d-flex justify-content-between">
-                                    <h4 class="mb-1 fw-bold fs-2"><?php echo htmlspecialchars($commento["usernameUtente"]); ?></h4>
+                                    <h4 class="mb-1 fw-bold fs-6"><?php echo htmlspecialchars($commento["usernameUtente"]); ?></h4>
                                     <small class="text-muted"><?php echo $commento["dataPubblicazione"]; ?></small>
                                 </div> 
 
